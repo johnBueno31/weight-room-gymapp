03d38e4bbd24844f7c181256dc144af88cfb08c5
John <johnranierbueno@gmail.com>
2024-05-20 22:42:32 -0700

    added features to Hero component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,24 @@
 function Hero() {
-	return <div>Hero</div>;
+	return (
+		<div className="min-h-screen flex flex-col gap-10 items-center justify-center text-center max-w-[800px] w-full mx-auto p-4">
+			<div className="flex flex-col">
+				<p>It's time to get</p>
+				<h1 className="uppercase font-semibold text-5xl sm:text-5xl md:text-6xl lg:text-7xl">
+					Swole<span className="text-blue-400">normous</span>
+				</h1>
+			</div>
+			<p className="text-sm md:text-base font-light">
+				Let us get shredded shall we? I hereby acknowledge that I may become{" "}
+				<span className="text-blue-400 font-medium">unbelieveably hyuuge</span>{" "}
+				and accept all risks of becoming the local{" "}
+				<span className="text-blue-400 font-medium">mass monstrosity</span>,
+				afflicted with severe body dysmorphia and unable to fit through doors.
+			</p>
+			<button className="px-8 px-4 rounded-md border-[2px] bg-slate-950 border-blue-400 border-solid blueShadow duration-200">
+				<p>Accept & Begin</p>
+			</button>
+		</div>
+	);
 }
 
 export default Hero;
